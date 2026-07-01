9709479ee7c97d562dbbee08ee590b70dd789d5a
dev <dev@macs-MacBook-Air.local>
2026-07-01 18:39:06 +0500

    refactor: rename application.css to scss and fix tests and fixtures

--- test/controllers/leads_controller_test.rb
+++ test/controllers/leads_controller_test.rb
@@ -2,17 +2,19 @@ require "test_helper"
 
 class LeadsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get leads_index_url
+    get leads_url
     assert_response :success
   end
 
   test "should get new" do
-    get leads_new_url
+    get new_lead_url
     assert_response :success
   end
 
-  test "should get create" do
-    get leads_create_url
-    assert_response :success
+  test "should create lead" do
+    assert_difference("Lead.count") do
+      post leads_url, params: { lead: { name: 'Test', email: 'test@example.com', message: 'Hello' } }
+    end
+    assert_redirected_to root_url
   end
 end
